b7b394d68fc2e028434bd2de0f8fdb367abb4a8b
Ivan Lyubchenko <lubchenko2044@gmail.com>
2022-01-21 01:52:40 +0300

    chenged publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: process.env.NODE_ENV === 'production' ? '/substring_searches/': '/'
+  publicPath: '/substring_searches/'
 }
