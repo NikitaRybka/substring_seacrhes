a14120a50aba978b1f30ca338df28c6471883ef9
Ivan Lyubchenko <lubchenko2044@gmail.com>
2022-01-21 01:28:13 +0300

    Added results page

---
+++ src/pages/test/results.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h3>Результаты</h3>
+    <div class="results" id="results">{{ correctAnswers }} / 5</div>
+    <v-btn
+      color="primary"
+      large
+      @click="$emit('restart')"
+    >Начать сначала</v-btn>
+  </div>
+</template>
+
+<script>
+import store from './store';
+
+export default {
+  computed: {
+    correctAnswers: {
+      get() {
+        return store.correctAnswers;
+      }
+    }
+  },
+  mounted() {
+    let resultsContainer = document.getElementById('results');
+    if(this.correctAnswers < 3) {
+      resultsContainer.style.color = 'red';
+    } else {
+      resultsContainer.style.color = 'green';
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  .results {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 90%;
+    font-size: 32px;
+  }
+</style>
