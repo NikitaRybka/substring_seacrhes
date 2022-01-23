c48c37efb87bb303a284d33f66176e31c2a307bc
NikitaRybka <98226897+NikitaRybka@users.noreply.github.com>
2022-01-23 22:42:26 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ git init
 git add -A 
 git commit -m "deploy"
 
-git push -f git@github.com:ivan-lyubchenko/substring_searches.git master:gh-pages
+git push -f git@github.com:NikitaRybka/substring_searches.git master:gh-pages
 cd -
